263c29c8ad6cb27650590fac2fb28c2ec1da29f0
Leonardo <rmnobarra@gmail.com>
2024-10-11 20:41:03 -0300

    many improvments

--- src/sound.py
+++ src/sound.py
@@ -4,32 +4,52 @@ import logging
 
 class SoundManager:
     def __init__(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        project_root = os.path.dirname(current_dir)
-        sound_dir = os.path.join(project_root, 'assets', 'sounds')
+        pygame.mixer.init()
+        self.sounds = {}
+        self.load_sounds()
 
-        self.background_music = self._load_sound(os.path.join(sound_dir, 'background_music.mp3'))
-        self.shoot_sound = self._load_sound(os.path.join(sound_dir, 'shoot.wav'))
+    def load_sounds(self):
+        sound_files = {
+            'background_music': 'background_music.mp3',
+            'presentation_music': 'presentation.mp3',
+            'shoot': 'shoot.wav',
+            'start': 'start.wav',
+            'win': 'win.wav',
+            'dead': 'dead.wav',
+            'game_over': 'game_over.wav',
+            'final_sunset': 'the_final_sunset.mp3'
+        }
 
-    def _load_sound(self, path):
-        try:
-            return pygame.mixer.Sound(path)
-        except FileNotFoundError:
-            logging.warning(f"Sound file not found: {path}")
-            return self._create_dummy_sound()
+        for sound_name, file_name in sound_files.items():
+            file_path = os.path.join('assets', 'sounds', file_name)
+            try:
+                if sound_name in ['background_music', 'presentation_music']:
+                    # We don't load these into self.sounds as they're handled differently
+                    pygame.mixer.music.load(file_path)
+                    logging.info(f"Loaded music: {sound_name}")
+                else:
+                    self.sounds[sound_name] = pygame.mixer.Sound(file_path)
+                    logging.info(f"Loaded sound: {sound_name}")
+            except pygame.error as e:
+                logging.error(f"Couldn't load sound {sound_name}: {e}")
 
-    def _create_dummy_sound(self):
-        buffer = bytearray(22050)  # 1 second of silence (22050 is the sample rate)
-        return pygame.mixer.Sound(buffer=buffer)
+    def play_sound(self, sound_name):
+        if sound_name in ['background_music', 'presentation_music']:
+            try:
+                pygame.mixer.music.load(os.path.join('assets', 'sounds', f'{sound_name}.mp3'))
+                pygame.mixer.music.play(-1)
+                logging.info(f"Playing music: {sound_name}")
+            except pygame.error as e:
+                logging.error(f"Couldn't play music {sound_name}: {e}")
+        elif sound_name in self.sounds:
+            try:
+                self.sounds[sound_name].play()
+                logging.info(f"Playing sound: {sound_name}")
+            except pygame.error as e:
+                logging.error(f"Couldn't play sound {sound_name}: {e}")
+        else:
+            logging.error(f"Sound not found: {sound_name}")
 
-    def play_background_music(self):
-        if self.background_music:
-            self.background_music.play(-1)  # -1 means loop indefinitely
-
-    def stop_background_music(self):
-        if self.background_music:
-            self.background_music.stop()
-
-    def play_shoot_sound(self):
-        if self.shoot_sound:
-            self.shoot_sound.play()
+    def stop_music(self):
+        pygame.mixer.music.stop()
+        logging.info("Stopped music")
